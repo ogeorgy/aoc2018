675a3021d955b6d3f9014372f02d65707fb9f33e
ogeorgy <georgy.noskov@gmail.com>
2018-12-04 22:15:58 +1000

    Add day01

---
+++ day01/part01.go
@@ -0,0 +1,31 @@
+// Advant Of Code 2018. https://adventofcode.com/2018.
+//
+// Day 1: Chronal Calibration
+// Part One: My puzzle answer was 510.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	data, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer data.Close()
+
+	answer := 0
+
+	scan := bufio.NewScanner(data)
+	for scan.Scan() {
+		value, _ := strconv.Atoi(scan.Text())
+		answer += value
+	}
+
+	fmt.Printf("Resulting frequency after all of the changes: %v\n", answer)
+}
